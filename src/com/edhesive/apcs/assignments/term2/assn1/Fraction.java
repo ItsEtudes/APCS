a57aa0b6f3be719f7b54b0ba62db3f47bfd18c54
ItsEtudes <jvbpit09>
2020-02-03 11:52:41 -0600

    assignment 2 javadoc

--- src/com/edhesive/apcs/assignments/term2/assn1/Fraction.java
+++ src/com/edhesive/apcs/assignments/term2/assn1/Fraction.java
@@ -1,16 +1,31 @@
 package com.edhesive.apcs.assignments.term2.assn1;
 
-/* Term 2 Assignment 1 - Fraction */
-/* A class which is used to represent fractions*/
+/**
+ * Assignment 2: Fraction
+ * This class is meant to represent a basic positive fraction.
+ * It stores a numerator and a denominator. It can be represented
+ * as a simple fraction string, or as a mixed number. There is also
+ * an addition operation.
+ */
 public class Fraction {
     private int numerator;
     private int denominator;
 
+    /**
+     * Default constructor, fraction = 1/1
+     */
     public Fraction() {
         numerator = 1;
         denominator = 1;
     }
 
+    /**
+     * Normal fraction constructor
+     * @param n numerator of the fraction. If not positive, this
+     *          is set to 0
+     * @param d denominator of the fraction. If not positive, this
+     *          is set to 0
+     */
     public Fraction(int n, int d) {
         if(n > 0) {
             numerator = n;
@@ -24,10 +39,21 @@ public class Fraction {
         }
     }
 
+    /**
+     * Allows the fraction to be represented as a String (Improper
+     * fraction format)
+     * @return String version of the fraction: "[numerator]/[denominator]"
+     */
     public String toString() {
         return "" + numerator + "/" + denominator;
     }
 
+    /**
+     * Same as toString(), but the fraction is represented in
+     * mixed number format
+     * @return String version of the fraction:
+     * "[whole number] [numerator]/denominator]"
+     */
     public String mixedNumber() {
         int whole = 0;
         while(numerator >= denominator) {
@@ -42,6 +68,12 @@ public class Fraction {
         return "" + whole;
     }
 
+    /**
+     * Adds another fraction to the fraction stored in the class.
+     * Both values must be positive
+     * @param n numerator of fraction to be added
+     * @param d denominator of fraction to be added
+     */
     public void add(int n, int d) {
         if(d > 0 && n > 0) {
             numerator = numerator * d + denominator * n;
