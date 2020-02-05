74ea38de214c2e187608b3fcb2685c645c0f2a91
Ben <bvazzana672@kirksville.k12.mo.us>
2020-02-04 23:24:29 -0600

    Lab 1: Mocking tone

--- src/com/edhesive/apcs/labs/lab1/ChatBotUtils.java
+++ src/com/edhesive/apcs/labs/lab1/ChatBotUtils.java
@@ -9,4 +9,17 @@ public class ChatBotUtils {
         return (int) (Math.random() * (max + 1)) + min;
     }
 
+    public static String convertToMockingTone(String message) {
+        message = message.toLowerCase();
+        String result = "";
+        for(int i = 0; i < message.length(); i++) {
+            String ch = message.substring(i, i + 1);
+            if(((int) (Math.random() * 2)) == 1) {
+                ch = ch.toUpperCase();
+            }
+            result += ch;
+        }
+        return result;
+    }
+
 }
