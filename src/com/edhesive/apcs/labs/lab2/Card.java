4afb3103b0b7d2987b5a4c7ce26b25aac1d391a8
Ben Vazzana <bvazzana672@kirksville.k12.mo.us>
2020-03-12 14:05:12 -0500

    Lab 2: Elevens (Activity 1: Card class)

--- src/com/edhesive/apcs/labs/lab2/Card.java
+++ src/com/edhesive/apcs/labs/lab2/Card.java
@@ -1,4 +1,41 @@
 package com.edhesive.apcs.labs.lab2;
 
 public class Card {
+
+    private String rank;
+    private String suit;
+
+    private int pointValue;
+
+    public Card(String rank, String suit, int pointValue) {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Card)) {
+            return false;
+        }
+        Card card = (Card) obj;
+        return rank.equals(card.rank) && suit.equals(card.suit) && pointValue == card.pointValue;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit + " (point value = " + pointValue + ")";
+    }
 }
